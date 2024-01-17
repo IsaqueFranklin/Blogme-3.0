456a4c4b330b0a7035071f9b8d1d4b2e16f6b2dd
Isaque <isaquefrankli@gmail.com>
2024-01-16 22:40:54 -0300

    Homepage userContext done

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,4 +1,45 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../UserContext';
+import { Navigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+
 export default function HomePage() {
+
+    const [redirect, setRedirect] = useState(null);
+
+    const {ready, user, setUser} = useContext(UserContext);
+
+    async function logout() {
+        await axios.post('/logout');
+        setRedirect('/');
+        setUser(null);
+    }
+
+    if (!ready) {
+        return 'Carregando...'
+    };
+
+    if (ready && !user && !redirect) {
+        return <Navigate to={'/login'} />
+    };
+
+    if (redirect) {
+        return <Navigate to={redirect} />
+    }
+
+    if (user) {
+        return (
+            <div>
+                <h2>{user.name}</h2>
+                <p>{user.email}</p>
+                <div className="text-center max-w-lg mx-auto">
+                    Logged in as {user.name} ({user.email}) <br/>
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>Home Page!</div>
     )
