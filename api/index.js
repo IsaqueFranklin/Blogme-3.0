59b113d262cc1c3d1f5d92f9d1420beaa9b27cc6
Isaque <isaquefrankli@gmail.com>
2024-02-08 19:27:25 -0300

    sending emails one recipient at time

--- api/index.js
+++ api/index.js
@@ -314,27 +314,30 @@ app.post('/enviar-email-teste', async (req, res) => {
       });
 
     // async..await is not allowed in global scope, must use a wrapper
-    async function main() {
-        // send mail with defined transport object
-        const info = await transporter.sendMail({
-        from: userData.email, // sender address
-        to: usuario.emailList, // list of receivers
-        subject: title, // Subject line
-        text: description, // plain text body
-        html: content, // html body
-        });
+    usuario.emailList.map(one => {
+        async function main() {
+            // send mail with defined transport object
+            const info = await transporter.sendMail({
+            from: userData.email, // sender address
+            to: one, // list of receivers
+            subject: title, // Subject line
+            text: description, // plain text body
+            html: content, // html body
+            });
+        
+            console.log("Message sent: %s", info.messageId);
+            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        
+            //
+            // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+            //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+            //       <https://github.com/forwardemail/preview-email>
+            //
+        }
+
+        main().catch(console.error);
+    })
     
-        console.log("Message sent: %s", info.messageId);
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-    
-        //
-        // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
-        //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
-        //       <https://github.com/forwardemail/preview-email>
-        //
-    }
-    
-    main().catch(console.error);
     res.json();
 })
 
