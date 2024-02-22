fd76994e48c22477a04cc5cd5e9ff5254297e1f4
Isaque <isaquefrankli@gmail.com>
2024-02-22 19:13:22 -0300

    vercel.json out

--- api/index.js
+++ api/index.js
@@ -28,9 +28,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
     origin: 'http://127.0.0.1:5173',
-    methods: ["POST", "GET"],
     credentials: true, 
-    optionSuccessStatus: 200
 }));
 
 //MongoDB connection goes here
