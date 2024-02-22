0abdbcd2d08ff687ffa41f0eddd3b6a66e5640b6
Isaque <isaquefrankli@gmail.com>
2024-02-22 12:58:26 -0300

    scripts fix

--- client/src/components/PostsGrid.jsx
+++ client/src/components/PostsGrid.jsx
@@ -7,15 +7,15 @@ export default function PostsGrid({places}) {
                 {places?.length > 0 && places?.map((post,i) => {
                     return (
                         <Link to={'/post/'+post._id+'/'+post.title.split(' ').join('-')} key={i}>
-                        <div className='bg-gray-500 mb-2 rounded-2xl flex'>
-                            {post.photos?.[0] && (
-                                <img className='rounded-2xl object-cover aspect-square' src={'http://localhost:4000/uploads/'+post.photos?.[0]} />
-                            )}
-                        </div>
-                        <h2 className='font-bold'>{post.title}</h2>
-                        <h3 className='text-sm text-gray-900'>{post.description}</h3> 
-                        {/*<div className='content' dangerouslySetInnerHTML={{__html:post.content}} /> */}   
-                    </Link>
+                            <div className='bg-gray-500 mb-2 rounded-2xl flex'>
+                                {post.photos?.[0] && (
+                                    <img className='rounded-2xl object-cover aspect-square' src={'http://localhost:4000/uploads/'+post.photos?.[0]} />
+                                )}
+                            </div>
+                            <h2 className='font-bold'>{post.title}</h2>
+                            <h3 className='text-sm text-gray-900'>{post.description}</h3> 
+                            {/*<div className='content' dangerouslySetInnerHTML={{__html:post.content}} /> */}   
+                        </Link>
                     )
                 })}
         </div>
